d34d4c47e412aec031733c2812b87f7d8f7a1c52
ayush soral <ayush.soral@gmail.com>
2018-05-12 15:45:43 +0530

    Add files via upload

--- project.py
+++ project.py
@@ -2,7 +2,8 @@ import cv2
 import os
 import numpy as np
 import sys
-from sklearn import svm
+from sklearn import svm,neural_network
+from sklearn.neural_network import MLPClassifier as nn
 import collections
 import pickle
 
@@ -20,16 +21,12 @@ for dirname,dirnames,filenames in os.walk("data/"):
 
             im = cv2.imread(os.path.join(subject_path, filename), cv2.IMREAD_GRAYSCALE)
             im = cv2.resize(im, (64, 128))
-            # try1 = np.asarray(im)
-            # cv2.imshow("blah", try1)
-            # cv2.waitKey(0)
-            # im1 = cv2.GaussianBlur(img,(5,5),0)
-            # im2 = cv2.GaussianBlur(img, (3, 3), 0)
-            # im=im2-im1
+            im = cv2.bilateralFilter(im, 8, 50, 50)
+
             im=hog.compute(im)
             X.append(im)
             y.append(c)
-
+        print subject_path
         c+=1
 
 
@@ -46,12 +43,65 @@ print X.shape
 print collections.Counter(y)
 print names
 
-clf=svm.SVC(C=1,kernel='rbf',probability=True)
+clf=svm.SVC(C=10,kernel='rbf',probability=True)
 clf.fit(np.asarray(X),np.asarray(y))
+
+filename = 'final.sav'
+pickle.dump(clf, open(filename, 'wb'))
+
+X_test,y_test=[],[]
+count=0
+c=0
+for dirname,dirnames,filenames in os.walk("test/"):
+    dirnames.sort()
+    for subdir in dirnames:
+        a=map(str,subdir.split('\n'))
+        names.append(a)
+        subject_path = os.path.join(dirname, subdir)
+        for filename in os.listdir(subject_path):
+
+            im = cv2.imread(os.path.join(subject_path, filename), cv2.IMREAD_GRAYSCALE)
+            im = cv2.resize(im, (64, 128))
+            im = cv2.bilateralFilter(im, 8, 50, 50)
+            im=hog.compute(im)
+            X_test.append(im)
+            # print im.shape
+            # # im=list(im)
+            # # print im
+            # nx, ny = im.shape
+            # im = im.reshape((nx * ny))
+            # print im.shape
+            # im.reshape(1,-1)
+            # print im.shape
+            # ans=clf.predict(im)
+
+            # print ans,c
+            # if c==ans:
+            #     count=count+1
+            y_test.append(c)
+        print subject_path
+        c+=1
+
+X_test=np.asarray(X_test)
+print X_test.shape
+
+nsamples, nx, ny = X_test.shape
+X_test = X_test.reshape((nsamples, nx * ny))
+ans=clf.predict(X_test)
+print ans,y_test
+for i in range(len(ans)):
+    if ans[i]==y_test[i]:
+        count+=1
+print count
+print (count*100.0)/len(ans)
+print collections.Counter(ans)
+prob=clf.predict_proba(X_test)
+
+# clf=nn(hidden_layer_sizes=(3780,1000,70,7),activation="relu",alpha=0.01,max_iter=1000)
+# clf.fit(np.asarray(X),np.asarray(y))
 # clf=cv2.face.LBPHFaceRecognizer_create()
 # clf.train(np.asarray(X),np.asarray(y))
-filename = 'rbfTrue.sav'
-pickle.dump(clf, open(filename, 'wb'))
+
 
 
 # params=[]
